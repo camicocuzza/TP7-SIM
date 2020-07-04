0d277f74bb79cb90ab99f2380f4dc2dc59b9a01e
Camila Cocuzza <78231@sistemasfrcutneduar.onmicrosoft.com>
2020-07-04 16:55:28 -0300

    ASDASF

--- TP7 SIM/Program.cs
+++ TP7 SIM/Program.cs
@@ -14,6 +14,7 @@ namespace TP7_SIM
         [STAThread]
         static void Main()
         {
+            D
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Formularios.Menu());
